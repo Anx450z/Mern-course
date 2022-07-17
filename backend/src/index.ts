8469d39431b2b815a9b5f821647601b2e7ea16ea
anx450z <anx450z@gmail.com>
2022-07-17 17:36:27 +0530

    add passport related checking

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,6 +8,9 @@ import session from "express-session";
 import bycrypt from "bcryptjs";
 import dotnet from "dotenv";
 import User from "./User";
+import { UserInterface } from "./Interface/UserInterface";
+
+const LocalStrategy = passportLocal.Strategy;
 
 mongoose.connect(
   "mongodb+srv://ankur:ankur@cluster0.pwl0a.mongodb.net/?retryWrites=true&w=majority",
@@ -38,6 +41,38 @@ app.use(cookieParser());
 app.use(passport.initialize());
 app.use(passport.session());
 
+//Passport
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ username: username }, (err : any, user: any) => {
+      if (err) throw err;
+      if (!user) return done(null, false);
+      bycrypt.compare(password, user.password, (err, result) => {
+        if (err) throw err;
+        if (result === true) {
+          return done(null, user);
+        } else {
+          return done(null, false);
+        }
+      });
+    });
+  })
+);
+
+passport.serializeUser((user: any, cb) => {
+  cb(null, user.id);
+});
+
+passport.deserializeUser((id: string, cb) => {
+  User.findOne({ _id: id }, (err : any, user: any) => {
+    const userInformation = {
+      username: user.username,
+      isAdmin: user.isAdmin,
+    };
+    cb(err, userInformation);
+  });
+});
+
 // Routes
 app.post("/register", async (req: Request, res: Response) => {
   // username, password validation
@@ -52,7 +87,7 @@ app.post("/register", async (req: Request, res: Response) => {
     return;
   }
   // Check user already exist
-  User.findOne({ username }, async (err: Error, doc: Document) => {
+  User.findOne({ username }, async (err: Error, doc: UserInterface) => {
     if (err) throw err;
     if (doc) res.send("user Already Exists");
     if (!doc) {
