b641ba7195ae8a2c3773814166c46b17e2058f18
anx450z <anx450z@gmail.com>
2022-07-17 19:19:15 +0530

    add Routes in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import NavBar from "./Components/NavBar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import AdminPage from "./Pages/AdminPage";
+import Login from "./Pages/Login";
+import Profile from "./Pages/Profile";
 
 function App() {
   return (
-    <div></div>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/admin" element={<AdminPage />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
