2d240d7aa04a842a3f5d9463d0b738ae302d9c7b
anx450z <anx450z@gmail.com>
2022-07-17 19:37:46 +0530

    add navbar style and use Link to

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import HomePage from "./Pages/HomePage";
 import AdminPage from "./Pages/AdminPage";
 import Login from "./Pages/Login";
 import Profile from "./Pages/Profile";
+import "./main.css";
 
 function App() {
   return (
--- client/src/Components/NavBar.tsx
+++ client/src/Components/NavBar.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function NavBar() {
   return (
-    <div>NavBar</div>
+    <div className='NavContainer'>
+      <Link to="/logout">Logout</Link>
+      <Link to="/home">Home</Link>
+      <Link to="/profile">Profile</Link>
+      <Link to="/admin">Admin</Link>
+      <Link to="/login">Login</Link>
+      <Link to="/register">Register</Link>
+    </div>
   )
 }
