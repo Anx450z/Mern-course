701c7f37be5a30b106064d265ca0d1ec9d5888ab
anx450z <anx450z@gmail.com>
2022-07-18 11:34:14 +0530

    add register page

--- client/src/Pages/Register.tsx
+++ client/src/Pages/Register.tsx
@@ -1,7 +1,48 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios, { AxiosResponse } from "axios";
 
 export default function Register() {
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  function register() {
+    axios
+      .post(
+        "http://localhost:4000/register",
+        {
+          username,
+          password,
+        },
+        {
+          withCredentials: true,
+        }
+      )
+      .then(
+        (res: AxiosResponse) => {
+          if (res.data === "success") {
+            window.location.href = "/login";
+          }
+        },
+        () => {
+          console.log("failure");
+        }
+      );
+  }
+
   return (
-    <div>Register</div>
-  )
+    <div>
+      <h1>Register</h1>
+      <input
+        type="text"
+        placeholder="username"
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={register}>Register</button>
+    </div>
+  );
 }
